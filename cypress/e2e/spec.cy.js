78c3c12c23afaa357cf29d309eb2bddaa4917e4d
your name <marika.lee@walmart.com>
2025-01-01 18:35:02 -0800

    update constants n5

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,141 +1,209 @@
 const url = "http://localhost:3000/sad"; //"https://sadqueue.github.io/sad/";
 
+
+// "4:00PM;17:00,17:00,17:00,17:00,17:00;5,4,3,2,1;DA>S2>S4>S1>S3",
+// "4:00PM;17:00,17:00,17:00,17:00,17:00;5,4,3,2,1;DA>S2>S4>S1>S3",
+// "4:00PM;17:00,17:00,17:00,17:00,17:00;5,4,3,2,1;DA>S2>S4>S1>S3",
+// "4:00PM;17:00,17:00,17:00,17:00,17:00;5,4,3,2,1;DA>S2>S4>S1>S3",
+// "4:00PM;17:00,17:00,17:00,17:00,17:00;5,4,3,2,1;DA>S2>S4>S1>S3",
+
+const testArrFourPm = [
+  // "7:00PM;17:00,17:00,17:00,17:00;5,4,3,2;S2>S4>N1>N2>N3>N4>S3>N5",
+  // "7:00PM;17:29,17:15,17:16,17:41;4,3,3,2;S3>S4>S2>N1>N2>N3>N4>N5",
+  "5:00PM;16:44,15:50,15:50,15:55;4,3,2,2;N5>S2>S3>S1>S4",
+  // "4:00PM;14:35,15:20,15:50,15:50,15:55;4,3,3,2,2;DA>S1>S2>S3>S4"
+]
+
+
 describe('template spec', () => {
-  it('4PM default', () => {
+  // it('4PM default', () => {
+  //   cy.visit(url);
+  //   cy.contains('Generate Queue').click();
+  //   cy.contains("Order 4:00PM");
+  //   cy.contains("DA>S1>S2>S3>S4");
+  // });
+
+  // it('5PM default', () => {
+  //   cy.visit(url);
+  //   cy.get('.timesdropdown').select('5:00PM');
+  //   cy.contains('Generate Queue').click();
+  //   cy.contains("Order 5:00PM");
+  //   cy.contains("S1>S2>S3>S4>N5");
+  // });
+
+  // it('7PM default', () => {
+  //   cy.visit(url);
+  //   cy.get('.timesdropdown').select('7:00PM');
+  //   cy.contains('Generate Queue').click();
+  //   cy.contains("Order 7:00PM");
+  //   cy.contains("S2>S3>S4>N5>N1>N2>N3>N4");
+  // });
+
+  // it('4PM change values', () => {
+  //   cy.visit(url);
+  //   cy.get('#numberOfAdmissions_0')
+  //     .clear()
+  //     .type("100")
+
+  //   cy.contains('Generate Queue').click();
+  //   cy.contains("Order 4:00PM");
+  //   cy.contains("S1>S2>S3>S4>DA");
+  // });
+
+  // it('4PM: DA has 100 admissions', () => {
+  //   cy.visit(url);
+  //   cy.get('#numberOfAdmissions_0')
+  //     .clear()
+  //     .type("100")
+
+  //   cy.contains('Generate Queue').click();
+  //   cy.contains("Order 4:00PM");
+  //   cy.contains("S1>S2>S3>S4>DA");
+  // });
+
+  // it('4PM: S3 is 1:30PM', () => {
+  //   cy.visit(url);
+
+  //   cy.get('#timestamp_3')
+  //     .clear()
+  //     .type("13:30")
+  //   cy.contains('Generate Queue').click();
+  //   cy.contains("Order 4:00PM");
+  //   cy.contains("S3>DA>S1>S2>S4");
+  // });
+
+
+  // it('5PM: S3 is 1:30PM', () => {
+  //   cy.visit(url);
+  //   cy.get('.timesdropdown').select('5:00PM');
+  //   cy.get('#timestamp_3')
+  //     .clear()
+  //     .type("13:30")
+  //   cy.contains('Generate Queue').click();
+  //   cy.contains("Order 5:00PM");
+  //   cy.contains("S4>S1>S2>S3>N5");
+  // });
+
+  // it('7PM: N4 is 12:00AM', () => {
+  //   cy.visit(url);
+  //   cy.get('.timesdropdown').select('7:00PM');
+  //   cy.get('#timestamp_3')
+  //     .clear()
+  //     .type("00:00")
+  //   cy.contains('Generate Queue').click();
+  //   cy.contains("Order 7:00PM");
+  //   cy.contains("N5>S2>S3>S4>N1>N2>N3>N4");
+  // });
+
+  // it('4PM: fill out all timestamps and number of admissions', () => {
+  //   cy.visit(url);
+  //   cy.get('.timesdropdown').select('4:00PM');
+
+  //   cy.get('#timestamp_0').clear().type("17:00")
+  //   cy.get('#timestamp_1').clear().type("16:00")
+  //   cy.get('#timestamp_2').clear().type("15:00")
+  //   cy.get('#timestamp_3').clear().type("14:00")
+  //   cy.get('#timestamp_4').clear().type("13:00")
+
+  //   cy.get('#numberOfAdmissions_0').clear().type("5")
+  //   cy.get('#numberOfAdmissions_1').clear().type("4")
+  //   cy.get('#numberOfAdmissions_2').clear().type("3")
+  //   cy.get('#numberOfAdmissions_3').clear().type("2")
+  //   cy.get('#numberOfAdmissions_4').clear().type("1")
+
+  //   cy.contains('Generate Queue').click();
+  //   cy.contains("Order 4:00PM");
+  //   cy.contains("S4>S2>DA>S3>S1");
+  // });
+
+  // it('5PM: fill out all timestamps and number of admissions', () => {
+  //   cy.visit(url);
+  //   cy.get('.timesdropdown').select('5:00PM');
+
+  //   cy.get('#timestamp_0').clear().type("17:00")
+  //   cy.get('#timestamp_1').clear().type("16:00")
+  //   cy.get('#timestamp_2').clear().type("15:00")
+  //   cy.get('#timestamp_3').clear().type("14:00")
+
+  //   cy.get('#numberOfAdmissions_0').clear().type("5")
+  //   cy.get('#numberOfAdmissions_1').clear().type("4")
+  //   cy.get('#numberOfAdmissions_2').clear().type("3")
+  //   cy.get('#numberOfAdmissions_3').clear().type("2")
+
+  //   cy.contains('Generate Queue').click();
+  //   cy.contains("Order 5:00PM");
+  //   cy.contains("N5>S4>S3>S2>S1");
+  // });
+
+  // it('7PM: fill out all timestamps and number of admissions', () => {
+  //   cy.visit(url);
+  //   cy.get('.timesdropdown').select('7:00PM');
+
+  //   cy.get('#timestamp_0').clear().type("17:00")
+  //   cy.get('#timestamp_1').clear().type("16:00")
+  //   cy.get('#timestamp_2').clear().type("15:00")
+  //   cy.get('#timestamp_3').clear().type("14:00")
+
+  //   cy.get('#numberOfAdmissions_0').clear().type("5")
+  //   cy.get('#numberOfAdmissions_1').clear().type("4")
+  //   cy.get('#numberOfAdmissions_2').clear().type("3")
+  //   cy.get('#numberOfAdmissions_3').clear().type("2")
+
+  //   cy.contains('Generate Queue').click();
+  //   cy.contains("Order 7:00PM");
+  //   cy.contains("S4>S2>N1>N2>N3>N4>N5>S3");
+  // });
+
+
+  it(`4PM: fill out all timestamps and number of admissions test loop`, () => {
     cy.visit(url);
-    cy.contains('Generate Queue').click();
-    cy.contains("Order 4:00PM");
-    cy.contains("DA>S1>S2>S3>S4");
+
+    for (let i=0; i<testArrFourPm.length; i++){     
+      const splitArr = testArrFourPm[i].split(";");
+
+      const time = splitArr[0];
+      const timestamps = splitArr[1].split(",");
+      const admissions = splitArr[2].split(",");
+      const output = splitArr[3];
+      cy.get('.timesdropdown').select(time);
+
+      timestamps.forEach((time, timeIndex)=>{
+        cy.get(`#timestamp_${timeIndex}`).clear().type(time)
+      })
+      admissions.forEach((admission, admissionIndex)=>{
+        cy.get(`#numberOfAdmissions_${admissionIndex}`).clear().type(admission)
+      })
+      cy.contains('Generate Queue').click();
+      cy.contains(`Order ${time}`);
+      cy.contains(output);
+    }
   });
 
-  it('5PM default', () => {
-    cy.visit(url);
-    cy.get('.timesdropdown').select('5:00PM');
-    cy.contains('Generate Queue').click();
-    cy.contains("Order 5:00PM");
-    cy.contains("S1>S2>S3>S4>N5");
-  });
+  // for (let i=0; testArrFourPm.length; i++){
+    // it(`7PM: fill out all timestamps and number of admissions test ${i}`, () => {
+      // cy.visit(url);
 
-  it('7PM default', () => {
-    cy.visit(url);
-    cy.get('.timesdropdown').select('7:00PM');
-    cy.contains('Generate Queue').click();
-    cy.contains("Order 7:00PM");
-    cy.contains("S2>S3>S4>N5>N1>N2>N3>N4");
-  });
+      // const splitArr = testArrFourPm.split(";");
+      // const time = splitArr(0);
+      // const timestamps = splitArr(1);
+      // const numberOfAdmissions = splitArr(2);
+      // const output = splitArr(3);
 
-  it('4PM change values', () => {
-    cy.visit(url);
-    cy.get('#numberOfAdmissions_0')
-      .clear()
-      .type("100")
-
-    cy.contains('Generate Queue').click();
-    cy.contains("Order 4:00PM");
-    cy.contains("S1>S2>S3>S4>DA");
-  });
-
-  it('4PM: DA has 100 admissions', () => {
-    cy.visit(url);
-    cy.get('#numberOfAdmissions_0')
-      .clear()
-      .type("100")
-
-    cy.contains('Generate Queue').click();
-    cy.contains("Order 4:00PM");
-    cy.contains("S1>S2>S3>S4>DA");
-  });
-
-  it('4PM: S3 is 1:30PM', () => {
-    cy.visit(url);
-
-    cy.get('#timestamp_3')
-      .clear()
-      .type("13:30")
-    cy.contains('Generate Queue').click();
-    cy.contains("Order 4:00PM");
-    cy.contains("S3>DA>S1>S2>S4");
-  });
-
-
-  it('5PM: S3 is 1:30PM', () => {
-    cy.visit(url);
-    cy.get('.timesdropdown').select('5:00PM');
-    cy.get('#timestamp_3')
-      .clear()
-      .type("13:30")
-    cy.contains('Generate Queue').click();
-    cy.contains("Order 5:00PM");
-    cy.contains("S4>S1>S2>S3>N5");
-  });
-
-  it('7PM: N4 is 12:00AM', () => {
-    cy.visit(url);
-    cy.get('.timesdropdown').select('7:00PM');
-    cy.get('#timestamp_3')
-      .clear()
-      .type("00:00")
-    cy.contains('Generate Queue').click();
-    cy.contains("Order 7:00PM");
-    cy.contains("N5>S2>S3>S4>N1>N2>N3>N4");
-  });
-
-  it('4PM: fill out all timestamps and number of admissions', () => {
-    cy.visit(url);
-    cy.get('.timesdropdown').select('4:00PM');
-
-    cy.get('#timestamp_0').clear().type("17:00")
-    cy.get('#timestamp_1').clear().type("16:00")
-    cy.get('#timestamp_2').clear().type("15:00")
-    cy.get('#timestamp_3').clear().type("14:00")
-    cy.get('#timestamp_4').clear().type("13:00")
-
-    cy.get('#numberOfAdmissions_0').clear().type("5")
-    cy.get('#numberOfAdmissions_1').clear().type("4")
-    cy.get('#numberOfAdmissions_2').clear().type("3")
-    cy.get('#numberOfAdmissions_3').clear().type("2")
-    cy.get('#numberOfAdmissions_4').clear().type("1")
-
-    cy.contains('Generate Queue').click();
-    cy.contains("Order 4:00PM");
-    cy.contains("S4>S2>DA>S3>S1");
-  });
-
-  it('5PM: fill out all timestamps and number of admissions', () => {
-    cy.visit(url);
-    cy.get('.timesdropdown').select('5:00PM');
-
-    cy.get('#timestamp_0').clear().type("17:00")
-    cy.get('#timestamp_1').clear().type("16:00")
-    cy.get('#timestamp_2').clear().type("15:00")
-    cy.get('#timestamp_3').clear().type("14:00")
-
-    cy.get('#numberOfAdmissions_0').clear().type("5")
-    cy.get('#numberOfAdmissions_1').clear().type("4")
-    cy.get('#numberOfAdmissions_2').clear().type("3")
-    cy.get('#numberOfAdmissions_3').clear().type("2")
-
-    cy.contains('Generate Queue').click();
-    cy.contains("Order 5:00PM");
-    cy.contains("N5>S4>S3>S2>S1");
-  });
-
-  it('7PM: fill out all timestamps and number of admissions', () => {
-    cy.visit(url);
-    cy.get('.timesdropdown').select('7:00PM');
-
-    cy.get('#timestamp_0').clear().type("17:00")
-    cy.get('#timestamp_1').clear().type("16:00")
-    cy.get('#timestamp_2').clear().type("15:00")
-    cy.get('#timestamp_3').clear().type("14:00")
-
-    cy.get('#numberOfAdmissions_0').clear().type("5")
-    cy.get('#numberOfAdmissions_1').clear().type("4")
-    cy.get('#numberOfAdmissions_2').clear().type("3")
-    cy.get('#numberOfAdmissions_3').clear().type("2")
-
-    cy.contains('Generate Queue').click();
-    cy.contains("Order 7:00PM");
-    cy.contains("S4>S2>N1>N2>N3>N4>N5>S3");
-  });
+      // cy.get('.timesdropdown').select(time);
+  
+      // timestamps.forEach((time, timeIndex)=>{
+      //   cy.get(`#timestamp_${timeIndex}`).clear().type(time)
+      // })
+      // admissions.forEach((admission, admissionIndex)=>{
+      //   // cy.get(`#timestamp_${timeIndex}`).clear().type(time)
+      //   cy.get(`#numberOfAdmissions_${timeInde}`).clear().type(admissions)
+      // })
+      // cy.contains('Generate Queue').click();
+      // cy.contains(`Order ${time}`);
+      // cy.contains(output);
+    // });
+    
+  // }
 })
+
+//4:00PM;17:00,17:00,17:00,17:00;5,4,3,2;S4>S2>N1>N2>N3>N4>N5>S3
