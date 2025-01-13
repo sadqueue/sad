62749ad5438d5c33c0358e676b4ede6ba8e74555
your name <marika.lee@walmart.com>
2025-01-12 17:51:52 -0800

    redo order of admissions so admissions greater than .67 will sort by timestamp

--- src/configuration.jsx
+++ src/configuration.jsx
@@ -1,27 +1,27 @@
-// Import the necessary Firebase modules
-import { initializeApp } from "firebase/app";
+// // Import the necessary Firebase modules
+// import { initializeApp } from "firebase/app";
 
-// Your Firebase config here
-const firebaseConfig = {
-  // apiKey: "YOUR_API_KEY",
-  // authDomain: "YOUR_AUTH_DOMAIN",
-  // projectId: "YOUR_PROJECT_ID",
-  // storageBucket: "YOUR_STORAGE_BUCKET",
-  // messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  // appId: "YOUR_APP_ID",
+// // Your Firebase config here
+// const firebaseConfig = {
+//   // apiKey: "YOUR_API_KEY",
+//   // authDomain: "YOUR_AUTH_DOMAIN",
+//   // projectId: "YOUR_PROJECT_ID",
+//   // storageBucket: "YOUR_STORAGE_BUCKET",
+//   // messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+//   // appId: "YOUR_APP_ID",
 
-    apiKey: "AIzaSyAphbWQLyVQCn5__ZbLH9JioF--g7MrgVs",
-    authDomain: "sad-queue.firebaseapp.com",
-    projectId: "sad-queue",
-    storageBucket: "sad-queue.firebasestorage.app",
-    messagingSenderId: "89448854015",
-    appId: "1:89448854015:web:38124c50c0efe134b4f7ca",
-    measurementId: "G-F3RZFBTDEY"
+//     apiKey: "AIzaSyAphbWQLyVQCn5__ZbLH9JioF--g7MrgVs",
+//     authDomain: "sad-queue.firebaseapp.com",
+//     projectId: "sad-queue",
+//     storageBucket: "sad-queue.firebasestorage.app",
+//     messagingSenderId: "89448854015",
+//     appId: "1:89448854015:web:38124c50c0efe134b4f7ca",
+//     measurementId: "G-F3RZFBTDEY"
 
-};
+// };
 
-// Initialize Firebase
- const cong = initializeApp(firebaseConfig);
+// // Initialize Firebase
+//  const cong = initializeApp(firebaseConfig);
 
-  export default cong;
-// Now you can use Firebase services in your React app!
+//   export default cong;
+// // Now you can use Firebase services in your React app!
