1375a3950802a095ec67a276869eb10b5443c084
your name <marika.lee@walmart.com>
2025-01-28 08:34:59 -0800

    push changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import HomePage from './HomePage'; // Your main page
-import VisualizationPage from './VisualizationPage'; // The new page
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+import VisualizationPage from './VisualizationPage';
 
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          {/* <li><Link to="/">Home</Link></li>
-          <li><Link to="/visualization">Visualization</Link></li> */}
-        </ul>
-      </nav>
-      
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/sad" element={<HomePage />} />
