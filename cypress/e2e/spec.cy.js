1bab66ae24317e69979ebfea2068ae671b908329
your name <marika.lee@walmart.com>
2025-01-31 14:46:12 -0800

    push changes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,4 @@
-const url = "http://localhost:3001/";//"https://sadqueue.github.io/sad/";//
+const url = "https://sadqueue.github.io/sad/";//"http://localhost:3001/";//
 
 const testArr5pm = [
 
@@ -169,67 +169,68 @@ describe('template spec', () => {
     cy.visit(url);
     let count = 1;
     
-    // cy.contains("Expand").click();
+    // cy.get("#seedetails").click();
+    // cy.contains("Set Composite Algorithm").click()
     // cy.get("#compositeScoreCheckbox").click();
 
     // cy.get(`#alr`).clear().type(0.7);
     // cy.get(`#clr`).clear().type(0.3);
 
-    // if (testArr5pm) {
-    //   /* test 5PM */
-    //   cy.get("#timesdropdown").should('be.visible').select("17:00");
-    //   cy.wait(1200);
-    //   for (let i = 0; i < testArr5pm.length; i++) {
-    //     const splitArr = testArr5pm[i][0].split(";");
+    if (testArr5pm) {
+      /* test 5PM */
+      cy.get("#timesdropdown").should('be.visible').select("17:00");
+      cy.wait(1200);
+      for (let i = 0; i < testArr5pm.length; i++) {
+        const splitArr = testArr5pm[i][0].split(";");
 
-    //     if (testArr5pm[i].length > 0) {
-    //       const timestamps = splitArr[0].split(",");
-    //       const admissions = splitArr[1].split(",");
-    //       const output = splitArr[2];
+        if (testArr5pm[i].length > 0) {
+          const timestamps = splitArr[0].split(",");
+          const admissions = splitArr[1].split(",");
+          const output = splitArr[2];
 
-    //       timestamps.forEach((time, timeIndexx) => {
-    //         const timeIndex = timeIndexx;
-    //         const admission = Number(admissions[timeIndexx]);
+          timestamps.forEach((time, timeIndexx) => {
+            const timeIndex = timeIndexx;
+            const admission = Number(admissions[timeIndexx]);
 
-    //         cy.get(`#timestamp_${timeIndexx}`).clear().type(time);
-    //         cy.get(`#numberOfAdmissions_${timeIndexx}`).clear().type(admission)
-    //       })
+            cy.get(`#timestamp_${timeIndexx}`).clear().type(time);
+            cy.get(`#numberOfAdmissions_${timeIndexx}`).clear().type(admission)
+          })
 
-    //       cy.get("#generateQueue").click();
+          cy.get("#generateQueue").click();
 
-    //       // cy.get('#alr') // Replace 'yourElementId' with the actual ID of the element
-    //       // .then(($el) => {
-    //       //   console.log("ALR: ", $el.text())
-    //       // });
-    //       // cy.get('#clr') // Replace 'yourElementId' with the actual ID of the element
-    //       // .then(($el) => {
-    //       //   console.log("CLR: ", $el.text())
-    //       // });
-    //       cy.get('#orderofadmissions_output') // Replace 'yourElementId' with the actual ID of the element
-    //         .then(($el) => {
+          // cy.get('#alr') // Replace 'yourElementId' with the actual ID of the element
+          // .then(($el) => {
+          //   console.log("ALR: ", $el.text())
+          // });
+          // cy.get('#clr') // Replace 'yourElementId' with the actual ID of the element
+          // .then(($el) => {
+          //   console.log("CLR: ", $el.text())
+          // });
+          cy.get('#orderofadmissions_output') // Replace 'yourElementId' with the actual ID of the element
+            .then(($el) => {
 
-    //           const generatedFromAutomation = $el.text() && $el.text().replace(/\(.*?\)/g, "").trim();
+              const generatedFromAutomation = $el.text() && $el.text().replace(/\(.*?\)/g, "").trim();
 
-    //           if (generatedFromAutomation && generatedFromAutomation == output.trim()) {
-    //             // console.log(testArr[i] + "::::::" + $el.text() + "----MATCHES");
-    //             // resArr.push(testArr[i] + "::::::" + $el.text() + "----MATCHES");
-    //           } else {
-    //             console.log("[" + count + "] 5PM " + testArr5pm[i][1] + " -- NO MATCH");
-    //             console.log("Data:        ", testArr5pm[i][0].split(";").slice(0, testArr5pm[i][0].split(";").length - 1).join(";"));
-    //             console.log("Manny:       ", output);
-    //             console.log("From UI:     ", $el.text());
-    //             count++;
-    //             // resArr.push(":::::: NO MATCH");
+              if (generatedFromAutomation && generatedFromAutomation == output.trim()) {
+                // console.log(testArr[i] + "::::::" + $el.text() + "----MATCHES");
+                // resArr.push(testArr[i] + "::::::" + $el.text() + "----MATCHES");
+              } else {
+                console.log("[" + count + "] 5PM " + testArr5pm[i][1] + " -- NO MATCH");
+                console.log("Data:        ", testArr5pm[i][0].split(";").slice(0, testArr5pm[i][0].split(";").length - 1).join(";"));
+                console.log("Manny:       ", output);
+                console.log("From UI:     ", $el.text());
+                count++;
+                // resArr.push(":::::: NO MATCH");
 
-    //           }
-    //           // console.log("does this log work? ",$el.text()); // Logs the value of the element
-    //         });
-    //     } else {
-    //       console.log("---- incorrect input", testArr5pm[i]);
-    //     }
+              }
+              // console.log("does this log work? ",$el.text()); // Logs the value of the element
+            });
+        } else {
+          console.log("---- incorrect input", testArr5pm[i]);
+        }
 
-    //   }
-    // }
+      }
+    }
 
     /* test 7PM */
     if (testArr7pm) {
