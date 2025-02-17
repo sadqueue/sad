f93386d9158cecbdbfe8e909192ebd0ca726f44d
your name <marika.lee@walmart.com>
2025-02-16 18:09:57 -0800

    push changes

--- src/HomePage.js
+++ src/HomePage.js
@@ -1219,13 +1219,18 @@ export function App() {
         }
         let increaseAlr = 0;
         if (!window.Cypress && dropdown == "19:00"){
-            lastSaved5Pm && lastSaved5Pm.shifts && lastSaved5Pm.shifts.forEach((fivePm, eachIndex)=>{
-                if (each.name == fivePm.name){
-                    if (fivePm.numberOfAdmissions !== "" && (Number(fivePm.numberOfAdmissions))+2 <= Number(each.numberOfAdmissions)){
-                        increaseAlr = Number(each.numberOfAdmissions) - Number(fivePm.numberOfAdmissions)-1;
+            if (each.name == "N5"){
+                increaseAlr = Number(each.numberOfAdmissions);
+            } else {
+                lastSaved5Pm && lastSaved5Pm.shifts && lastSaved5Pm.shifts.forEach((fivePm, eachIndex)=>{
+                    if (each.name == fivePm.name){
+                        if (fivePm.numberOfAdmissions !== "" && (Number(fivePm.numberOfAdmissions))+2 <= Number(each.numberOfAdmissions)){
+                            increaseAlr = Number(each.numberOfAdmissions) - Number(fivePm.numberOfAdmissions)-1;
+                        }
                     }
-                }
-            })
+                })
+            }
+            
         }
         
         const originalAlr = Number(1 - (fixedDiff) / p95);
